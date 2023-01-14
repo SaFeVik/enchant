e7fce26645bff81fbb6afd75c3db1f34ba2c990d
Felix Vik <felixhusbanan@gmail.com>
2023-01-15 00:16:47 +0100

    mistakes osv

--- enchant.js
+++ enchant.js
@@ -1,6 +1,7 @@
 let galaxEl = document.querySelector('#galax')
 let normEl = document.querySelector('#norm')
 let wrongEl = document.querySelector('#wrong')
+let rightEl = document.querySelector('#right')
 let infoEl = document.querySelector('#info')
 let keyboardEl = document.querySelector('#keyboard')
 
@@ -9,6 +10,7 @@ function sleep(ms) {
 }
 
 let wrong = 0
+let right = 0
 let bokstaver = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 let bokstav = bokstaver[Math.floor(Math.random()*26)]
 galaxEl.innerHTML = bokstav
@@ -20,12 +22,16 @@ document.addEventListener('keyup', async (e) =>{
     if (normEl.innerHTML == galaxEl.innerHTML){
         infoEl.innerHTML = `Correct`
         galaxEl.innerHTML = bokstav
+        right += 1
+        rightEl.innerHTML = `You have ${right} right in a row`
         await sleep(1000)
         normEl.innerHTML = "-"
     }
     else{
         infoEl.innerHTML = `Wrong`
         wrong += 1
+        right = 0
+        rightEl.innerHTML = `You have ${right} right in a row`
         wrongEl.innerHTML = `You have ${wrong} mistakes`
     }
 })
