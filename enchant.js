493c41b66973e94fff9a78a4198f1326913b875e
Felix Vik <felixhusbanan@gmail.com>
2023-01-15 17:38:24 +0100

    flere bokstaver

--- enchant.js
+++ enchant.js
@@ -1,4 +1,5 @@
 let galaxEl = document.querySelector('#galax')
+let galax1El = document.querySelector('#galax1')
 let wrongEl = document.querySelector('#wrong')
 let rightEl = document.querySelector('#right')
 let infoEl = document.querySelector('#info')
@@ -11,14 +12,21 @@ function sleep(ms) {
 let wrong = 0
 let right = 0
 let bokstaver = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-let bokstav = bokstaver[Math.floor(Math.random()*26)]
-galaxEl.innerHTML = bokstav
+let bokstav = bokstaver[Math.floor(Math.random()*26)]+bokstaver[Math.floor(Math.random()*26)]+bokstaver[Math.floor(Math.random()*26)]
+
+galaxEl.innerHTML = bokstav.charAt(0)+bokstav.charAt(1)
+galax1El.innerHTML = bokstav.charAt(2)
+
+console.log(bokstav.charAt(0), bokstav.charAt(1), bokstav.charAt(2))
+
 document.addEventListener('keyup', async (e) =>{
-    bokstav = bokstaver[Math.floor(Math.random()*26)]
-    console.log(normEl.value, galaxEl.innerHTML, normEl.value == galaxEl.innerHTML)
-    if (normEl.value == galaxEl.innerHTML){
+    console.log("norm:",normEl.value,"galax:", bokstav.charAt(0),bokstav.charAt(1), bokstav.charAt(2), e.key == bokstav.charAt(2))
+
+    if (e.key == bokstav.charAt(2)){
+        bokstav = bokstaver[Math.floor(Math.random()*26)]+bokstav.charAt(0)+bokstav.charAt(1)+bokstav.charAt(2)
         infoEl.innerHTML = `Correct`
-        galaxEl.innerHTML = bokstav
+        galaxEl.innerHTML = bokstav.charAt(0)+bokstav.charAt(1)
+        galax1El.innerHTML = bokstav.charAt(2)
         right += 1
         rightEl.innerHTML = `You have ${right} right in a row`
     }
@@ -29,5 +37,7 @@ document.addEventListener('keyup', async (e) =>{
         rightEl.innerHTML = `You have ${right} right in a row`
         wrongEl.innerHTML = `You have ${wrong} mistakes`
     }
+})
+document.addEventListener('keydown', async (e) =>{
     normEl.value = ""
 })
